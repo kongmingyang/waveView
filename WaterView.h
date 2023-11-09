dc4d25d9c334e847e25a14cf1c89f3187e734bdc
孔明 <310155154@qq.com>
2023-11-09 15:05:37 +0800

    Add files via upload

---
+++ WaterView.h
@@ -0,0 +1,17 @@
+//
+//  WaterView.h
+//  WaterWave
+//
+//  Created by 明孔 on 2020/4/7.
+//  Copyright © 2020 明孔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WaterView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WaterView.m
@@ -0,0 +1,21 @@
+//
+//  WaterView.m
+//  WaterWave
+//
+//  Created by 明孔 on 2020/4/7.
+//  Copyright © 2020 明孔. All rights reserved.
+//
+
+#import "WaterView.h"
+
+@implementation WaterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
