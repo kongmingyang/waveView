43623a0f65db2fc2cbea220b658477721d57c76b
孔明 <310155154@qq.com>
2023-12-07 16:15:08 +0800

    Update WaterView.h

--- WaterView.h
+++ WaterView.h
@@ -1,17 +1,17 @@
 //
-//  WaterView.h
+//  WaveView.h
 //  WaterWave
 //
-//  Created by 明孔 on 2020/4/7.
-//  Copyright © 2020 明孔. All rights reserved.
+//  Created by 中电兴发 on 2023/11/8.
+//  Copyright © 2023 明孔. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface WaterView : UIView
-
+@interface WaveView : UIView
+- (void)setUp; 
 @end
 
 NS_ASSUME_NONNULL_END
